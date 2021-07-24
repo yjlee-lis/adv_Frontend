c680402a664316dba6c5569a4779ef87cb81a8fb
yjlee-lis <68377953+yjlee-lis@users.noreply.github.com>
2021-07-24 22:20:08 +0900

    modify page name

--- getID.js
+++ getID.js
@@ -3,13 +3,13 @@ let newsID = 458;
 function set_id_single(news_id){
     newsID = news_id;
     console.log('set-newsid', news_id);
-    location.href = "blog-single.html?"+news_id;
+    location.href = "news_single.html?"+news_id;
 }
 
 function set_id_pin(news_id){
     newsID = news_id;
     console.log('set-newsid', news_id);
-    location.href = "pricing.html?"+news_id;
+    location.href = "pin.html?"+news_id;
 }
 
 function get_News_id(){
